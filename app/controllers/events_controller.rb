a05b2eb392eb95145dc1f26cbdc61c6fb7fd9333
Ezra Rush <rushwest@gmail.com>
2015-04-30 16:15:36 -0500

    bootstrap style

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -86,6 +86,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :description, :start_time, :end_time, :user_id)
+      params.require(:event).permit(:title, :description, :start_time, :end_time, :user_id, :all_day)
     end
 end
